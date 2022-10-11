72a2a657ce08e3e22632915907fe1c165fe809cc
Kamil Poręba <kamilporeba@hotmail.com>
2022-10-11 14:23:14 +0200

    prisma added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # quiz-system-backend
+
+Project made for Internet Application classes.
+
+## Prisma - commands
+
+---
+
+Create migration:
+
+    npx prisma migrate dev --name init
+
+---
+
+Generate prisma client
+
+    npx prisma generate
--- api/index.ts
+++ api/index.ts
@@ -6,12 +6,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server sss is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
