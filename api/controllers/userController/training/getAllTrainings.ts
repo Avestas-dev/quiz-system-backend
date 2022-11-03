fb74f867fb1ccdf21a914a02de90bdf74c634c03
Avestas-dev <66955268+Avestas-dev@users.noreply.github.com>
2022-11-03 01:54:37 +0100

    Liked implemented in getAllTrainings endpoint.

--- api/controllers/userController/training/getAllTrainings.ts
+++ api/controllers/userController/training/getAllTrainings.ts
@@ -6,11 +6,17 @@ export const getAllTrainings = async (req: Request, res: AuthResponse) => {
     #swagger.tags = ['Training']
     #swagger.description = 'Gets all trainings of all user that have visiblity set to true, and all trainings of logged in user.'
     #swagger.security = [{"apiKeyAuth": []}]
+     #swagger.parameters['onlyLiked'] = {
+            in: 'query',
+            description: 'Set to true, if only liked one should be displayed',
+            required: true,
+    }  
     #swagger.responses[200] = {
       description: 'All trainings received.',
       schema: { $ref: '#/definitions/GetAllTrainingsResponse' }
     }    
   */
+  const onlyLiked = req.query?.onlyLiked === "true";
   const allTrainings = await prisma.training.findMany({
     where: {
       OR: [
@@ -29,14 +35,17 @@ export const getAllTrainings = async (req: Request, res: AuthResponse) => {
       LikeTraining: true,
     },
   });
+
   return res.json(
-    allTrainings.map((al) => ({
-      ...al,
-      TagTraining: al.TagTraining.map((e) => ({
-        tagId: e.tagId,
-        tagName: e.tag.name,
-      })),
-      LikeTraining: !!al.LikeTraining.length,
-    }))
+    allTrainings
+      .map((al) => ({
+        ...al,
+        TagTraining: al.TagTraining.map((e) => ({
+          tagId: e.tagId,
+          tagName: e.tag.name,
+        })),
+        LikeTraining: !!al.LikeTraining.length,
+      }))
+      .filter((e) => (onlyLiked ? e.LikeTraining : true))
   );
 };
