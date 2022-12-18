83067331ced95f4c6bee8a6e87d433dafa7948e2
Avestas-dev <66955268+Avestas-dev@users.noreply.github.com>
2022-12-18 22:26:23 +0100

    Profile updated.

--- api/controllers/userController/profile.ts
+++ api/controllers/userController/profile.ts
@@ -2,16 +2,18 @@ import { Request } from "express";
 import { ProfileResponseModel } from "../../models/profileModel";
 
 export const profile = async (req: Request, res: ProfileResponseModel) => {
-  /* 	#swagger.tags = ['Auth']
-        #swagger.description = 'Get user profile from token'
-        #swagger.security = [{"apiKeyAuth": []}]
-        #swagger.responses[200] = {
-            description: 'User profile.',
-            schema: { $ref: '#/definitions/ProfileResponse' }
-        }
+  /* 	
+      #swagger.tags = ['Auth']
+      #swagger.description = 'Get user profile from token'
+      #swagger.security = [{"apiKeyAuth": []}]
+      #swagger.responses[200] = {
+          description: 'User profile.',
+          schema: { $ref: '#/definitions/ProfileResponse' }
+      }
   */
   return res.json({
     id: res.locals.user.id,
     email: res.locals.user.email,
+    isAdmin: res.locals.user.isAdmin,
   });
 };
--- api/models/profileModel.ts
+++ api/models/profileModel.ts
@@ -3,11 +3,12 @@ import { Response } from "express";
 import { extractResBody } from "../helpers/typescriptHelpers";
 import { commonLocals } from "./commonLocals";
 
-type ProfileLocals = Pick<User, "email" | "id">;
+type ProfileLocals = Pick<User, "email" | "id" | "isAdmin">;
 
 export type ProfileResponseModel = Response<ProfileLocals, commonLocals>;
 
 export const ProfileResponseExample: extractResBody<ProfileResponseModel> = {
   email: "kamilporeba@hotmail.com",
   id: 1,
+  isAdmin: true,
 };
